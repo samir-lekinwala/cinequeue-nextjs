b202305355f3a61b911ccf6eed747015766ccec9
Samir Lekinwala <samirlekinwala@gmail.com>
2024-09-23 18:06:38 +1200

    Create some api calls for movie data to test with

--- app/components/MoviesCarousel.jsx
+++ app/components/MoviesCarousel.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getData } from '../api/apiCalls'
 
 function MoviesCarousel() {
-  return <div className="text-green-500">MoviesCarousel</div>
+  const [moviesContent, setMoviesContent] = useState([])
+
+  // const movieCarouselContent = [
+  //   {
+  //     type: 'Top Rated',
+  //     content: getData('movie/now_playing?language=en-US&page=1'),
+  //   },
+  // ]
+
+  async function getMoviesContentData() {
+    const inCinemas = await getData('movie/now_playing?language=en-US&page=1')
+    // setMoviesContent(inCinemas.results)
+    const popular = await getData('movie/popular?language=en-US&page=1')
+    // setMoviesContent(popular.results)
+
+    const lessThan90MinuteRuntime = await getData(
+      'discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_runtime.lte=90'
+    )
+
+    const dataCollection = [
+      { type: 'In Cinemas', data: inCinemas },
+      { type: 'Popular', data: popular },
+      { type: 'Less than 90 Minutes', data: lessThan90MinuteRuntime },
+    ]
+    setMoviesContent(dataCollection)
+
+    // const inCinemas = await getData('movie/now_playing?language=en-US&page=1')
+    // setMoviesContent(inCinemas.results)
+  }
+
+  useEffect(() => {
+    getMoviesContentData()
+  }, [])
+
+  console.log('movies content test', moviesContent)
+  return (
+    <div className="text-green-500">
+      {!moviesContent ? (
+        <p>Movies loading...</p>
+      ) : (
+        moviesContent.map((type) => (
+          <>
+            <p className="text-2xl" key={type.type}>
+              {type.type}
+            </p>
+            {/* {console.log('type', type.data)} */}
+            {type.data.results.map((movie) => (
+              <p className="text-white" key={movie.id}>
+                {movie.title}
+              </p>
+            ))}
+          </>
+        ))
+      )}
+    </div>
+  )
 }
 
 export default MoviesCarousel
