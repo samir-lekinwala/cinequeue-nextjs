6c8fa72911d43bd1fb703dec364e0b5e3399d2a6
Samir Lekinwala <samirlekinwala@gmail.com>
2024-09-30 15:00:35 +1300

    Add animation for view more on carousel posters

--- app/components/SingleItemContent.jsx
+++ app/components/SingleItemContent.jsx
@@ -6,7 +6,7 @@ function SingleItemContent({ content, classes, type }) {
   const [hoverState, setHoverState] = useState(false)
 
   function hoverStateChangeTrue() {
-    setHoverState(true)
+    setHoverState(!hoverState)
   }
   function hoverStateChangeFalse() {
     setHoverState(false)
@@ -19,28 +19,27 @@ function SingleItemContent({ content, classes, type }) {
 
   return (
     <div
-      onMouseEnter={hoverStateChangeTrue}
-      onMouseLeave={hoverStateChangeFalse}
+      onClick={hoverStateChangeTrue}
+      // oncl={hoverStateChangeFalse}
       className={`flex flex-col w-[200px] ${classes} overflow-hidden`}
     >
       {hoverState ? (
-        <div className="w-[200px] h-[300px] p-2 overflow-y-auto overflow-x-hidden absolute opacity-100 z-10 text-gray-400">
-          <div>
-            {' '}
+        <div className="w-[200px] h-[300px] overflow-y-auto overflow-x-hidden absolute opacity-100 z-10 text-gray-400">
+          <div className="w-full">
             <Link href={`/${type}/${content.id}`}>
-              <button className="bg-white bg-opacity-10 text-white">
+              <button className="animate-gradient-animation-expand rounded-lg h-[1.5rem] duration-500 bg-opacity-10 text-white fixed right-0 left-0 top-0 hover:scale-105 hover:font-normal ease-in-out">
                 View More
               </button>
             </Link>
           </div>
-          {content.overview}
+          <p className="pt-[1.5rem]">{content.overview}</p>
         </div>
       ) : null}
       <div
         className={`${
           hoverState
-            ? 'opacity-10 transition-all duration-500 scale-125 '
-            : null
+            ? 'opacity-10 transition-all duration-500 scale-125 ease-in-out'
+            : 'transition-all duration-500 scale-100 ease-in-out'
         } `}
       >
         <img
@@ -52,7 +51,7 @@ function SingleItemContent({ content, classes, type }) {
       </div>
       <div key={content.id}>
         <p className=" text-center font-poppins text-gray-400">
-          {content.title}
+          <Link href={`/${type}/${content.id}`}>{content.title}</Link>
         </p>
         <p className=" text-center font-poppins text-gray-400 text-sm">
           {content.release_date}
