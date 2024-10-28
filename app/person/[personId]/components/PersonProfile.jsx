2720d77e03059d98eb212bbfd039b82c91c0bbc5
Samir Lekinwala <samirlekinwala@gmail.com>
2024-10-28 16:40:28 +1300

    tv credits now combines characters under the same tv show id for the actor.

--- app/person/[personId]/components/PersonProfile.jsx
+++ app/person/[personId]/components/PersonProfile.jsx
@@ -1,7 +1,183 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SingleItemContent from '../../../components/SingleItemContent'
 
-function PersonProfile() {
-  return <div>PersonProfile</div>
+function PersonProfile({ data }) {
+  const [biographyButtonClick, setBiographyButtonClick] = useState(false)
+  const [yearsOld, setYearsOld] = useState(0)
+  const [movieCredits, setMovieCredits] = useState([])
+  const [tvCredits, setTvCredits] = useState([])
+
+  const biographyOpen = () => setBiographyButtonClick(true)
+  const biographyClose = () => setBiographyButtonClick(false)
+
+  function calculateAge(birthDate, otherDate) {
+    birthDate = new Date(birthDate)
+    otherDate = new Date(otherDate)
+    console.log(birthDate, otherDate)
+    let years = otherDate.getFullYear() - birthDate.getFullYear()
+
+    if (
+      otherDate.getMonth() < birthDate.getMonth() ||
+      (otherDate.getMonth() == birthDate.getMonth() &&
+        otherDate.getDate() < birthDate.getDate())
+    ) {
+      years--
+    }
+
+    return years
+  }
+
+  useEffect(() => {
+    if (data) {
+      if (data.deathday) {
+        setYearsOld(calculateAge(data.birthday, data.birthday))
+      } else if (data.birthday) {
+        const todaysDate = new Date()
+        setYearsOld(calculateAge(data.birthday, todaysDate))
+      }
+      const sortedMoviesByDate = data.movie_credits.cast.toSorted((a, b) => {
+        const aTest = new Date(a.release_date)
+        const bTest = new Date(b.release_date)
+
+        return bTest - aTest
+      })
+      setMovieCredits(sortedMoviesByDate)
+
+      function combineDuplicateId() {
+        const characterMap = {}
+        //get the id of the show, if the id is the same add the character to the characterArray.
+        const tvCreditsArray = data.tv_credits.cast
+        // const tvCreditsArray = data.tv_credits.cast.toSorted((a, b) => a.id - b.id)
+
+        if (tvCreditsArray) {
+          for (let i = 0; i < tvCreditsArray.length; i++) {
+            // console.log('index', i, tvCreditsArray[i + 1])
+            if (
+              characterMap[tvCreditsArray[i].id]
+              //stuck here...
+            ) {
+              characterMap[tvCreditsArray[i].id].push(
+                tvCreditsArray[i].character
+              )
+            } else {
+              characterMap[tvCreditsArray[i].id] = [tvCreditsArray[i].character]
+            }
+          }
+
+          console.log('combined deplicate test', characterMap)
+        }
+
+        let newSortedTvCredits = []
+
+        for (const key in characterMap) {
+          if (characterMap[key].length > 1) {
+            console.log('length longer than 1', key, characterMap[key])
+            // tvCreditsArray[key]
+            const filterDuplicate = tvCreditsArray.filter(
+              (item) => item.id == key
+            )
+            const removedDuplicates = tvCreditsArray.filter(
+              (item) => item.id != key
+            )
+
+            filterDuplicate[0].character = characterMap[key].join(', ')
+            removedDuplicates.push(filterDuplicate[0])
+            // removedDuplicates.push()
+            newSortedTvCredits = [...removedDuplicates]
+            console.log('sorted by id second test', filterDuplicate)
+            console.log('sorted by id third test', removedDuplicates)
+          }
+        }
+
+        const sortedTvByDate = newSortedTvCredits.toSorted((a, b) => {
+          const aTest = new Date(a.first_air_date)
+          const bTest = new Date(b.first_air_date)
+
+          return bTest - aTest
+        })
+
+        console.log('new sorted tv credits', sortedTvByDate)
+        return sortedTvByDate
+      }
+
+      setTvCredits(combineDuplicateId())
+    }
+  }, [data])
+
+  return data ? (
+    <div className="p-4">
+      <div className=" sm:flex-row flex flex-col gap-2">
+        <img
+          className="sm:w-1/2 max-h-[400px] sm:max-h-[400px] object-contain rounded-xl"
+          src={`https://image.tmdb.org/t/p/w500/${data.profile_path}`}
+          alt=""
+        />
+        <div className="text-white sm:w-full flex flex-col gap-2">
+          <div className="text-2xl animate-gradient-animation-text text-transparent text-center">
+            {data.name}
+          </div>
+          <div>{yearsOld} Years old</div>
+          {data.deathday ? (
+            <div>
+              {data.birthday} - {data.deathday}
+            </div>
+          ) : (
+            <div>{data.birthday}</div>
+          )}
+          <div>{data.place_of_birth}</div>
+          <div
+            onClick={!biographyButtonClick ? biographyOpen : biographyClose}
+            className={`text-sm ${
+              biographyButtonClick ? 'line-clamp-none' : 'line-clamp-6'
+            }`}
+          >
+            {data.biography}
+          </div>
+          <div className="text-transparent animate-gradient-animation-text">
+            {!biographyButtonClick ? (
+              <button onClick={biographyOpen}>Read More</button>
+            ) : (
+              <button onClick={biographyClose}>Read Less</button>
+            )}
+          </div>
+        </div>
+      </div>
+      <div>
+        Movies
+        <div className="flex justify-center flex-wrap gap-2">
+          {movieCredits?.map((movie) => (
+            <div key={movie.id}>
+              <span className="text-white w-full object-contain">
+                {movie.character}
+              </span>
+              <SingleItemContent
+                content={movie}
+                type={'movie'}
+                classes={'rounded-xl'}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="">
+        <span className="text-white text-2xl">TV</span>
+        <div className="flex justify-center flex-wrap gap-2">
+          {tvCredits?.map((tv) => (
+            <div key={tv.credit_id}>
+              <span className="text-white w-full object-contain">
+                {tv.character}
+              </span>
+              <SingleItemContent
+                content={tv}
+                type={'tv'}
+                classes={'rounded-xl'}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  ) : null
 }
 
 export default PersonProfile
