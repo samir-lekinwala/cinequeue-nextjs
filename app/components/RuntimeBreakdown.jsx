776da1c3917b41389f9694104d2978f6dc548a91
Samir Lekinwala <samirlekinwala@gmail.com>
2024-11-09 19:54:46 +1300

    Added number of episodes aried to the runtimebreakdown using a new lastepisodetoair function

--- app/components/RuntimeBreakdown.jsx
+++ app/components/RuntimeBreakdown.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { getAverageRuntimeFromSeason1 } from '../functions/tvShowRuntime'
+import { lastEpisodeToAir } from '../functions/lastEpisodeToAir'
 
 function RuntimeBreakdown({
   content,
@@ -7,6 +8,10 @@ function RuntimeBreakdown({
   runtimeClick,
   setRuntimeClick,
 }) {
+  const lastEpisodeToAirData = lastEpisodeToAir(content)
+
+  console.log('from poster section last episode', lastEpisodeToAirData)
+
   //useEffect that handles when outside of ref is clicked to close the runtime details
   useEffect(() => {
     if (runtimeClick) {
@@ -29,6 +34,7 @@ function RuntimeBreakdown({
     <div className=" absolute  backdrop-blur-md border bg-black bg-opacity-30 border-black shadow-2xl w-fit text-nowrap rounded-lg p-4 translate-x-[-60px]">
       <div className="flex flex-col items-center">
         <p>Total number of episodes: {content.number_of_episodes}</p>
+        <p>Episodes Aired: {lastEpisodeToAirData}</p>
         <p>Seasons: {content.number_of_seasons}</p>
         <p>
           Episode average runtime:{' '}
