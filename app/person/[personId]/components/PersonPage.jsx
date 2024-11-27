0a888a78e69a01d92e15e567880e4f09ee16a059
Samir Lekinwala <samirlekinwala@gmail.com>
2024-11-28 09:23:19 +1300

    Fix import path for getdata on persons page

--- app/person/[personId]/components/PersonPage.jsx
+++ app/person/[personId]/components/PersonPage.jsx
@@ -1,7 +1,7 @@
 'use client'
 import { useParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
-import { getData } from '../../../api/apiCalls'
+import { getData } from '../../../lib/apiCalls'
 import PersonProfile from './PersonProfile'
 import useDocumentTitle from '../../../hooks/useDocumentTitle'
 
