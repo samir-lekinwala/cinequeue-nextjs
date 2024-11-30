887ea4e180ab0d11e39842025f3c36b1c0db7f49
Samir Lekinwala <samirlekinwala@gmail.com>
2024-11-30 21:00:07 +1300

    Remove blocked out code

--- app/api/radarr/route.ts
+++ app/api/radarr/route.ts
@@ -45,7 +45,6 @@ export async function GET(req: NextRequest, { params }) {
       // Return JSON response using NextResponse
       console.log('hello tmdb id detected', data)
       return Response.json(data)
-      // http://192.168.178.176:7878/api/v3/movie?tmdbId=27205&apikey=438168f831174e489373f3bb1ed6fcd3
     } else {
       const response = await fetch(
         `${RADARR_API_URL}/api/v3/${query}?apikey=${apiKey}`
